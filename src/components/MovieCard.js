582adacd591c821b108f86386f93f3a8ec48d476
Benjamin Odisho <Semiroundpizza8@gmail.com>
2022-10-30 23:06:11 -0500

    front-end done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const cardStyles = {
     maxWidth: '150px'
@@ -7,12 +8,14 @@ const cardStyles = {
 
 const MovieCard = ({ movie }) => {
     return (
+        <Link to={`/movies/${movie.id}`}>
         <Card style={cardStyles}>
             <CardContent>
                 <Typography variant="h5">{movie.name}</Typography>
                 <Typography variant="subheader" color="text.secondary">RUNTIME: {movie.runtime}</Typography>
             </CardContent>
         </Card>
+        </Link>
     );
 };
 
--- src/components/SingleMoviePage.js
+++ src/components/SingleMoviePage.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import { CircularProgress } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const SingleMoviePage = () => {
+    const [selectedMovie, setSelectedMovie] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const { movieId } = useParams();
+
+    const fetchMoviesInfo = async () => {
+        try {
+            setLoading(true);
+            const movie = await axios.get(`/api/movies/${movieId}`);
+            setSelectedMovie(movie.data);
+            setLoading(false);
+        } catch (err) {
+            setHasError(true);
+        }
+    }
+
+    useEffect(() => {
+        fetchMoviesInfo();
+    }, []);
+
+    if (hasError) return <h1>Oh no, things broke!</h1>
+    if (loading) return <CircularProgress />
+
     return (
         <div>
-            <h1>Single Movie</h1>
+            <h1>{selectedMovie.name}</h1>
+            <h2>{selectedMovie.runtime}</h2>
         </div>
     );
 };
